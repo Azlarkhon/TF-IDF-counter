b0148ced0b3bb0df51a641325200ff9f22ec3831
Azlarkhon <xasanxojaevazlarxon2004@gmail.com>
2025-06-12 01:35:15 +0500

    fix: добавить cors

--- middleware/corsMiddleware.go
+++ middleware/corsMiddleware.go
@@ -5,7 +5,17 @@ import (
 )
 
 func CorsMiddleware(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "http://37.9.53.45:8080")
+	origin := c.Request.Header.Get("Origin")
+
+	allowedOrigins := map[string]bool{
+		"http://37.9.53.45:8080": true,
+		"http://localhost:8080":  true,
+	}
+
+	if allowedOrigins[origin] {
+		c.Header("Access-Control-Allow-Origin", origin)
+	}
+
 	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	c.Header("Access-Control-Allow-Credentials", "true")
