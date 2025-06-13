c26d98da81e8411fecd144b10a22375f09a959e8
Azlarkhon <xasanxojaevazlarxon2004@gmail.com>
2025-06-13 14:51:35 +0500

    fix: разделить маршруты

--- routes/metricsRoute.go
+++ routes/metricsRoute.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"tfidf-app/controllers"
 	"tfidf-app/database"
-	"tfidf-app/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,12 +10,5 @@ import (
 func MetricsRoute(r *gin.Engine) {
 	metricsController := controllers.NewMetricsController(database.DB)
 
-	r.GET("/", controllers.ShowUploadForm)
 	r.GET("/metrics", metricsController.GetMetrics)
-
-	protected := r.Group("/")
-	protected.Use(middleware.AuthMiddleware)
-	{
-		protected.POST("/upload", controllers.HandleFileUpload)
-	}
 }
